0125e9661af12ee7a883e4350844abafad10149e
Eddie Hernandez <mchh.004@gmail.com>
2020-01-08 20:27:10 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song 
+  attr_accessor :name, :artist, :genre,
+  
+  @@count = 0 
+  
+  def initializer(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count += 1 
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genres 
+    
+  
+end
